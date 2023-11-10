e5fc006fc0178494987fb0b40372a98b5a5611c2
a-riceeater <elijb12@gmail.com>
2023-11-09 20:38:10 -0800

    socket

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  // Handle audio streaming logic here
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
